eab556609b072aab34832642a6f15ed56d671a5e
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2024-08-30 15:57:29 +0600

    first commit

---
+++ condition.js
@@ -0,0 +1,53 @@
+
+// Condition Prectice //
+
+
+
+// node condition.js//
+var taka = 550;
+if (taka > 430) {
+    // console.log('Tumar Taka Thik Ace');
+}
+else {
+    // console.log('Tumar Taka Thik nai');
+}
+
+// node condition.js//
+
+var num = 50
+
+if (num < 25) {
+    // console.log("tor matha thik nai");
+}
+else {
+    // console.log("tor matha thik ace");
+}
+
+// node condition.js//
+
+const apple = 280;
+const s = 200;
+
+if (apple < 200) {
+    console.log("tumi Aro kicu taka niye aso");
+}
+
+else if (apple <= 300) {
+    // console.log("tumi Amr Kach Theke Apple Kinte Parbe");
+
+}
+else {
+    console.log("Tumi pokhi");
+}
+
+
+// node condition.js//
+
+if (13 < 10){
+    console.log("kire koi");
+}
+else {
+    console.log(" Asci Ami");
+}
+
+// node condition.js//
